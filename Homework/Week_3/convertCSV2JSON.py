77b4e400753d026d09701cbb96c43fc91e3f01e2
Tula Kaptein <tula.kaptein@gmail.com>
2018-11-21 15:18:51 +0100

    Homework week 3

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -8,11 +8,11 @@ This script converts a .csv file to a .JSON file
 
 import pandas as pd
 
-INPUT_CSV = "lifeExpectancyAtBirth.csv"
-OUTPUT = "lifeExpectancyAtBirth.json"
+INPUT_CSV = "data.csv"
+OUTPUT = "data.json"
 
 df = pd.read_csv(INPUT_CSV, sep=';', na_values='', usecols=['Country', 'Region',
         'Income Group', 'Year', 'Life Expectancy'])
+df['Life Expectancy'] = df['Life Expectancy'].str.replace(',', '.')
 
-df.to_csv("test.csv")
 df.to_json(OUTPUT, orient='index')
