0d319f13bbfc2546572d50b61282f46f74097426
Tula Kaptein <tula.kaptein@gmail.com>
2018-12-03 15:46:38 +0100

    update

--- Homework/Week_5/scatter.js
+++ Homework/Week_5/scatter.js
@@ -30,8 +30,17 @@ function main (response){
               .attr("width", width + margin.left + margin.right)
               .attr("height", height + margin.top + margin.bottom);
 
-  xScale.domain([0,80]);
-  yScale.domain([0,4000]);
+  maxX = d3.max(dataset, function(d){
+    return parseInt(d.lifeExpectancy);
+  })
+  maxY = d3.max(dataset, function(d){
+    return parseInt(d.deathRate);
+  })
+  console.log(maxX)
+  console.log(maxY)
+
+  xScale.domain([0,maxX]);
+  yScale.domain([0,maxY]);
 
   // Make x and y axis
   var xAxis = d3.axisBottom(xScale);
@@ -54,15 +63,15 @@ function main (response){
      .text("Average life expectancy (years)");
 
   svg.append("text")
-     .attr("x", -210)
+     .attr("transform", "rotate(-90)")
+     .attr("x", -230)
      .attr("y", 60)
      .attr("class", "axis-title")
-     .attr("transform", "rotate(-90)")
      .text("Age adjusted death rate (per 100.000)");
 
  legend = svg.append("rect")
              .attr("class", "legend")
-             .attr("x", 400)
+             .attr("x", width - 100)
              .attr("y", margin.top)
              .attr("width", 100)
              .attr("height", 40);
@@ -72,26 +81,75 @@ function main (response){
   var colors = ['rgb(27,158,119)','rgb(217,95,2)','rgb(117,112,179)'];
   var legendData = [];
 
-  makeScatter("Both Sexes")
+  var points = makeScatter("Both Sexes")
 
-  d3.select('#x-axis-menu')
-    .selectAll('li')
+  d3.select("#menu")
+    .selectAll("button")
     .data(sexes)
     .enter()
-    .append('li')
-    .text(function(d) {return d;})
-    .on('click', function(d) {
-      return updateScatter(d)
-    });
+    .append("button")
+    .text(function(d){
+      return d;
+    })
+    .on('click', function(d){
+      updatePlot(d);
+      return console.log("Whoop");
+    })
+
+    // function updatePlot(sex){
+    //   for (item in races){
+    //     let array = makeArray(dataset, sex, races[item]);
+    //     svg.selectAll(races[item])
+    //        .data(array)
+    //        .transition()
+    //        .duration(1000)
+    //        .ease(d3.easeQuadOut)
+    //        // .attr("fill", color[item])
+    //        .attr("r", 1.5)
+    //        .attr("cx", function(d){
+    //          return xScale(d.lifeExpectancy) + margin.left
+    //        })
+    //        .attr("cy", function(d){
+    //          return yScale(d.deathRate) + margin.top
+    //        })
+    //
+    //   }
+    // }
+
+  function updatePlot(sex){
+    for (item in races){
+      let t = d3.transition().duration(1000).ease(d3.easeQuadOut);
+      let array = makeArray(dataset, sex, races[item]);
+      var points = svg.selectAll("." + races[item])
+                      .data(array);
+      console.log(points)
+      // console.log(sex, races[item])
+      points.enter()
+            .append("circle")
+            .attr("r", 1.5)
+            .attr("fill", colors[item])
+            .attr("cy", height)
+            .attr("cx", 0)
+            .attr("class", races[item])
+            .merge(points)
+            .transition(t)
+            .attr("cx", function(d){
+              return xScale(d.lifeExpectancy) + margin.left
+            })
+            .attr("cy", function(d){
+              return yScale(d.deathRate) + margin.top
+            })
+    }
+  }
 
   function makeScatter(sex){
     for (item in races){
       let array = makeArray(dataset, sex, races[item]);
-      var points = svg.selectAll("point")
+      let points = svg.selectAll("point")
                       .data(array)
                       .enter()
                       .append("circle")
-                      .attr("class", "point")
+                      .attr("class", races[item])
                       .attr("cx", function(d){
                         return xScale(d.lifeExpectancy) + margin.left
                       })
@@ -104,54 +162,37 @@ function main (response){
       legendData.push(legendItem);
     }
     makeLegend(legendData);
-  }
-
-  function updateScatter(sex){
-
+    return points
   }
 
   function makeLegend(legendData){
-    for (item in legendData){
-      svg.append("circle")
-            .attr("class", "point")
-            .attr("fill", legendData[item].color)
-            .attr("cx", 410)
-            .attr("cy", margin.top + 10 + 10*item)
-            .attr("r", 3);
-      svg.append("text")
-         .attr("x", 430)
-         .attr("y", margin.top + 13 + 10*item)
-         .attr("font-size", "10px")
-         .text(legendData[item].name);
-    }
-  }
-
-  // svg.selectAll("circle")
-  //    .data(legendData)
-  //    .enter()
-  //    .append("circle")
-  //    .attr("class", "circle")
-  //    .attr("fill", function(d){
-  //      return d.color
-  //    })
-  //    .attr("cx", 410)
-  //    .attr("cy", function(d, i){
-  //      return margin.top + 10 + 10*i
-  //    })
-  //    .attr("r", 3)
-  // svg.selectAll("text")
-  //    .data(legendData)
-  //    .enter()
-  //    .append("text")
-  //    .attr("x", 430)
-  //    .attr("y", function(d,i){
-  //      return margin.top + 13 + 10*i
-  //    })
-  //    .attr("font-size", "10px")
-  //    .text(function(d){
-  //      return d.name
-  //    })
-
+    svg.selectAll("legendPoint")
+       .data(legendData)
+       .enter()
+       .append("circle")
+       .attr("class", "legendPoint")
+       .attr("fill", function(d){
+         return d.color
+       })
+       .attr("cx", width - 90)
+       .attr("cy", function(d, i){
+         return margin.top + 10 + 10*i
+       })
+       .attr("r", 3)
+    svg.selectAll("legendText")
+       .data(legendData)
+       .enter()
+       .append("text")
+       .attr("class", "legendText")
+       .attr("x", width - 70)
+       .attr("y", function(d,i){
+         return margin.top + 13 + 10*i
+       })
+       .attr("font-size", "10px")
+       .text(function(d){
+         return d.name
+       })
+   }
 }
 
 function transformResponse(data){
